28f410026a3043a70d33576a9a9c7c17cd85c98b
Cihathan <mhmtonar1@gmail.com>
2022-03-29 19:28:16 +0300

    update

--- src/main/java/com/softengine/jwttest/config/AutoConfigure.java
+++ src/main/java/com/softengine/jwttest/config/AutoConfigure.java
@@ -1,9 +1,11 @@
 package com.softengine.jwttest.config;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan("com.softengine.jwttest")
+@EnableAutoConfiguration
 public class AutoConfigure {
 }
