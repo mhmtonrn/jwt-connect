539b2b3cad8d9cadd9e1560606223598e5d15982
Cihathan <mhmtonar1@gmail.com>
2022-03-29 19:33:12 +0300

    update

--- src/main/java/com/softengine/jwttest/config/AutoConfigure.java
+++ src/main/java/com/softengine/jwttest/config/AutoConfigure.java
@@ -1,11 +1,12 @@
 package com.softengine.jwttest.config;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan("com.softengine.jwttest")
-@EnableAutoConfiguration
+@ConditionalOnClass
 public class AutoConfigure {
 }
