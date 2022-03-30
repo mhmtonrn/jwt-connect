9a1660f7ad90a3202f0b7cd3d62e940785c677d9
mhmtonrn <mhmtonar1@gmail.com>
2022-03-30 16:22:20 +0300

    Create JwtConfig.java

---
+++ src/main/java/com/softengine/jwttest/config/JwtConfig.java
@@ -0,0 +1,10 @@
+package com.softengine.jwttest.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.softengine.jwttest")
+public class JwtConfig {
+
+}
